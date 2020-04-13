88fcd48b994fd24fe50947c06534f60bd815a5c7
heav3nz <jurg_92@hotmail.com>
2020-04-12 20:26:48 -0700

    Diseñador de los reportes sin listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+declare var Stimulsoft: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'hvz-resportDesigner';
+  options: any;
+  designer: any;
+
+  ngOnInit() {
+
+    this.options = new Stimulsoft.Designer.StiDesignerOptions();
+    this.options.appearance.fullScreenMode = true;
+
+    this.designer = new Stimulsoft.Designer.StiDesigner(this.options, 'StiDesigner', false);
+
+    this.designer.report = new Stimulsoft.Report.StiReport();
+    this.designer.report.loadFile('../reports/Inventory.mrt');
+    this.designer.renderHtml('designer');
+    }
 }
